a3dfdccee9cb519220777eecf09949b90b0bf42f
KoichiDevs <125679139+KoichiDevs@users.noreply.github.com>
2023-03-01 15:32:16 +0800

    Update App.jsx
    HELMET

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,18 @@ import History from './components/History'
 import Roadmap from './components/Roadmap'
 import Tokenomics from './components/Tokenomics'
 import Container from './templates/Container'
+import { Helmet } from 'react-helmet'
 
 const App = () => {
 
     return (
         <>
-        
+            <Helmet>
+            <script async type="module" id="embrCheckout" src="https://scripts.embr.org/checkout/checkout.js" data-chain="ethereum" data-address="0xf0a3a52eef1ebe77bb2743f53035b5813afe721f">
+    </script>
+            </Helmet>
             <div className='z-50 relative'>
                 <Hero />
-
             </div>
             <History />
             <Tokenomics />
