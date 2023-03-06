615dab1dd5f90762d41971a2bd2c9b79d9a64dd1
KoichiDevs <125679139+KoichiDevs@users.noreply.github.com>
2023-03-06 09:16:29 +0800

    Update hero.jsx

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -55,7 +55,7 @@ const Hero = () => {
             link: '',
             logo:
                 <>
-                    <FaBNB />
+                    <FaBinance />
                 </>
         },
         {
