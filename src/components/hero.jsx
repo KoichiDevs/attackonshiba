e42da28c22cb269e74ac4bfcf43200d39b3ee867
KoichiDevs <125679139+KoichiDevs@users.noreply.github.com>
2023-03-06 09:14:06 +0800

    Update hero.jsx

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -55,7 +55,7 @@ const Hero = () => {
             link: '',
             logo:
                 <>
-                    <FaAirbnb />
+                    <FaBNB />
                 </>
         },
         {
